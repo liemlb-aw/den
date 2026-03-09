f921c8a6f06cffffd6467616dda42a3bd5146e0f
LiemLB <liemlb1608@outlook.com>
2026-03-09 21:27:49 +0700

    remove flake-file

--- nix/dendritic.nix
+++ nix/dendritic.nix
@@ -1,8 +1,8 @@
 { inputs, lib, ... }:
 {
 
-  flake-file.inputs.flake-aspects.url = lib.mkDefault "github:vic/flake-aspects";
-  flake-file.inputs.den.url = lib.mkDefault "github:vic/den";
+  # flake-file.inputs.flake-aspects.url = lib.mkDefault "github:vic/flake-aspects";
+  # flake-file.inputs.den.url = lib.mkDefault "github:vic/den";
 
   imports = [
     (inputs.den.flakeModule or { })
